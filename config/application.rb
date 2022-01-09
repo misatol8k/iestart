20e5582b3f7e32575e66a7056b4a8a3b583bb182
misatol8k <misatolkurihara@gmail.com>
2022-01-09 22:28:41 +0900

    rails g でHelperのファイルを自動生成しない設定

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,10 @@ module Iestart
     config.load_defaults 6.0
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja    
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.helper false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
