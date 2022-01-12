f4084153a3a9d3aa76688db21d41e8ce684bc605
misatol8k <misatolkurihara@gmail.com>
2022-01-13 00:53:45 +0900

    propertyのCRUD機能を追加

--- app/controllers/property_controller.rb
+++ app/controllers/property_controller.rb
@@ -1,10 +1,37 @@
 class PropertyController < ApplicationController
+  before_action :set_property, only: %i[ show destroy ]
+  
   def index
+    @property = Property.all
   end
 
   def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+
+    respond_to do |format|
+      if @property.save
+        format.html { redirect_to @property, notice: "Property was successfully created." }
+        format.json { render :show, status: :created, location: @property }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @property.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
+
+  end
+  private
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:type, :trader)
   end
 end
