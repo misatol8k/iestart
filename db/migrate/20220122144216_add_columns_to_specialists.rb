6412d1c3023cf6bb63e6ec9271a73e0c6d36a2e9
misatol8k <misatolkurihara@gmail.com>
2022-01-23 00:11:59 +0900

    専門家テーブルにマッチング用カラムを追加

---
+++ db/migrate/20220122144216_add_columns_to_specialists.rb
@@ -0,0 +1,15 @@
+class AddColumnsToSpecialists < ActiveRecord::Migration[6.0]
+  def change
+      add_column :specialists, :newly_built_house, :integer, null: false
+      add_column :specialists, :used_house, :integer, null: false
+      add_column :specialists, :newly_built_condominium, :integer, null: false
+      add_column :specialists, :used_condominium, :integer, null: false
+      add_column :specialists, :land, :integer, null: false
+      add_column :specialists, :price_and_pepayment, :integer, null: false
+      add_column :specialists, :building_and_structure, :integer, null: false
+      add_column :specialists, :floor_plan, :integer, null: false
+      add_column :specialists, :surrounding_environment, :integer, null: false
+      add_column :specialists, :family_relatives, :integer, null: false
+      add_column :specialists, :other_point, :integer, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_070124) do
+ActiveRecord::Schema.define(version: 2022_01_22_144216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,17 @@ ActiveRecord::Schema.define(version: 2022_01_18_070124) do
     t.string "string"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "newly_built_house", null: false
+    t.integer "used_house", null: false
+    t.integer "newly_built_condominium", null: false
+    t.integer "used_condominium", null: false
+    t.integer "land", null: false
+    t.integer "price_and_pepayment", null: false
+    t.integer "building_and_structure", null: false
+    t.integer "floor_plan", null: false
+    t.integer "surrounding_environment", null: false
+    t.integer "family_relatives", null: false
+    t.integer "other_point", null: false
   end
 
 end
