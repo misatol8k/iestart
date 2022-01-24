d384493211766239e3b9be39781d65d3854e2dac
misatol8k <misatolkurihara@gmail.com>
2022-01-25 02:09:46 +0900

    マイグレーションファイルにデフォルト値を設定

--- db/migrate/20220123083539_add_columns_to_properties.rb
+++ db/migrate/20220123083539_add_columns_to_properties.rb
@@ -1,15 +1,15 @@
 class AddColumnsToProperties < ActiveRecord::Migration[6.0]
   def change
-    add_column :properties, :newly_built_house, :integer, null: false
-    add_column :properties, :used_house, :integer, null: false
-    add_column :properties, :newly_built_condominium, :integer, null: false
-    add_column :properties, :used_condominium, :integer, null: false
-    add_column :properties, :land, :integer, null: false
-    add_column :properties, :price_and_pepayment, :integer, null: false
-    add_column :properties, :building_and_structure, :integer, null: false
-    add_column :properties, :floor_plan, :integer, null: false
-    add_column :properties, :surrounding_environment, :integer, null: false
-    add_column :properties, :family_relatives, :integer, null: false
-    add_column :properties, :other_point, :integer, null: false    
+    add_column :properties, :newly_built_house, :integer, null: false, default: 0
+    add_column :properties, :used_house, :integer, null: false, default: 0
+    add_column :properties, :newly_built_condominium, :integer, null: false, default: 0
+    add_column :properties, :used_condominium, :integer, null: false, default: 0
+    add_column :properties, :land, :integer, null: false, default: 0
+    add_column :properties, :price_and_pepayment, :integer, null: false, default: 0
+    add_column :properties, :building_and_structure, :integer, null: false, default: 0
+    add_column :properties, :floor_plan, :integer, null: false, default: 0
+    add_column :properties, :surrounding_environment, :integer, null: false, default: 0
+    add_column :properties, :family_relatives, :integer, null: false, default: 0
+    add_column :properties, :other_point, :integer, null: false, default: 0
   end
 end
