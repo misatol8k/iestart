bd68d1d9f17939b16227f8a6355e6516c713cae5
misatol8k <misatolkurihara@gmail.com>
2022-01-24 23:59:49 +0900

    seedデータを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Property.create(property_type: 1, url: "url/bukkenjouhou1.jp", other: "sonota1", trader: "XXhome", price: 30000, newly_built_house:1 , used_house:0 , newly_built_condominium:0 , used_condominium:0 , land:0 , price_and_pepayment:1 , building_and_structure:0 , floor_plan:1 , surrounding_environment:0 , family_relatives:1, other_point:0 )
-Property.create(property_type: 2, url: "url/bukkenjouhou2.jp", other: "sonota2", trader: "XXhome", price: 50000, newly_built_house:0 , used_house:1 , newly_built_condominium:0 , used_condominium:0 , land:0 , price_and_pepayment:1 , building_and_structure:0 , floor_plan:0 , surrounding_environment:1 , family_relatives:0, other_point:0 )
-Property.create(property_type: 3, url: "url/bukkenjouhou3.jp", other: "sonota3", trader: "XXhome", price: 110000, newly_built_house:0 , used_house:0 , newly_built_condominium:1 , used_condominium:0 , land:0 , price_and_pepayment:0 , building_and_structure:0 , floor_plan:1 , surrounding_environment:1 , family_relatives:0, other_point:0 )
 
 Specialist.create(name:"専門家の名前1", expertise_property:"得意な物件です", consultation_item:"得意な相談項目です", qualification:"資格の内容です", description:"専門家の説明です。", career:"専門家の経歴です。", newly_built_house:1 , used_house:1 , newly_built_condominium:1 , used_condominium:1 , land:1 , price_and_pepayment:1 , building_and_structure:1 , floor_plan:1 , surrounding_environment:1 , family_relatives:1, other_point:1 )
 Specialist.create(name:"専門家の名前2", expertise_property:"得意な物件です", consultation_item:"得意な相談項目です", qualification:"資格の内容です", description:"専門家の説明です。", career:"専門家の経歴です。", newly_built_house:2 , used_house:2 , newly_built_condominium:5 , used_condominium:5 , land:3 , price_and_pepayment:2 , building_and_structure:3 , floor_plan:1 , surrounding_environment:1 , family_relatives:4, other_point:2 )
@@ -25,3 +22,10 @@ ConsultationMatter.create(item:"間取り")
 ConsultationMatter.create(item:"周辺環境")
 ConsultationMatter.create(item:"家族・親族")
 ConsultationMatter.create(item:"その他")
+
+Customer.create(name: "kokyaku1", mail: "kokyaku1@mail.com")
+Customer.create(name: "kokyaku2", mail: "kokyaku2@mail.com")
+
+Property.create(property_type: 1, url: "url/bukkenjouhou1.jp", other: "sonota1", trader: "XXhome", price: 30000, newly_built_house:1 , used_house:0 , newly_built_condominium:0 , used_condominium:0 , land:0 , price_and_pepayment:1 , building_and_structure:0 , floor_plan:1 , surrounding_environment:0 , family_relatives:1, other_point:0, customer_id:1 )
+Property.create(property_type: 2, url: "url/bukkenjouhou2.jp", other: "sonota2", trader: "XXhome", price: 50000, newly_built_house:0 , used_house:1 , newly_built_condominium:0 , used_condominium:0 , land:0 , price_and_pepayment:1 , building_and_structure:0 , floor_plan:0 , surrounding_environment:1 , family_relatives:0, other_point:0, customer_id:2 )
+Property.create(property_type: 3, url: "url/bukkenjouhou3.jp", other: "sonota3", trader: "XXhome", price: 110000, newly_built_house:0 , used_house:0 , newly_built_condominium:1 , used_condominium:0 , land:0 , price_and_pepayment:0 , building_and_structure:0 , floor_plan:1 , surrounding_environment:1 , family_relatives:0, other_point:0 )
