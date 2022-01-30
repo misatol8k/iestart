6d7947493f727e8e218b6fb41ccfe781010eca13
emi ota <ametio.145@gmail.com>
2022-01-30 06:32:35 +0000

    メール送信機能を修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,7 @@
 class ContactMailer < ApplicationMailer
-  def contact_mail(contact)
+  def contact_mail(customer)
     @customer = customer
-    mail to: @customer.email, subject: "お問い合わせの確認メール"
+    # mail to: @customer.mail, subject: "お問い合わせの確認メール"
+    mail to: 'default@gmail.com', subject: "お問い合わせの確認メール"
   end
 end
