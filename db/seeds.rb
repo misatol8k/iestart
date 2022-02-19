79f09ee8e94db234a9e4a1d5a5b4b211ed8a611c
misatol8k <misatolkurihara@gmail.com>
2022-02-20 00:14:28 +0900

    seedデータを変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,45 +10,46 @@
 # Specialist.create(name:"専門家の名前2", expertise_property:"得意な物件です", consultation_item:"得意な相談項目です", qualification:"資格の内容です", description:"専門家の説明です。", career:"専門家の経歴です。", newly_built_house:2 , used_house:2 , newly_built_condominium:5 , used_condominium:5 , land:3 , price_and_pepayment:2 , building_and_structure:3 , floor_plan:1 , surrounding_environment:1 , family_relatives:4, other_point:2 )
 # Specialist.create(name:"専門家の名前3", expertise_property:"得意な物件です", consultation_item:"得意な相談項目です", qualification:"資格の内容です", description:"専門家の説明です。", career:"専門家の経歴です。", newly_built_house:5 , used_house:4 , newly_built_condominium:3 , used_condominium:3 , land:5 , price_and_pepayment:4 , building_and_structure:2 , floor_plan:5 , surrounding_environment:4 , family_relatives:4, other_point:1 )
 
-require "csv"
-CSV.foreach('db/iestart_specialists.csv', headers: true) do |row|
-    Specialist.create(
-    name: row[1],
-    expertise_property:"得意な物件です", 
-    consultation_item:"得意な相談項目です", 
-    qualification:row[16], 
-    description:row[17], 
-    career:row[18], 
-    newly_built_house:row[4], 
-    used_house:row[5] , 
-    newly_built_condominium:row[6] , 
-    used_condominium:row[7] , 
-    land:row[8] , 
-    price_and_pepayment:row[9] , 
-    building_and_structure:row[10]  , 
-    floor_plan:row[11]  , 
-    surrounding_environment:row[12]  , 
-    family_relatives:row[13] , 
-    other_point:row[14] 
-  )
-end
+# require "csv"
+# CSV.foreach('db/iestart_specialists.csv', headers: true) do |row|
+#     Specialist.create(
+#     name: row[1],
+#     expertise_property:"得意な物件です", 
+#     consultation_item:"得意な相談項目です", 
+#     qualification:row[16], 
+#     description:row[17], 
+#     career:row[18], 
+#     newly_built_house:row[4], 
+#     used_house:row[5] , 
+#     newly_built_condominium:row[6] , 
+#     used_condominium:row[7] , 
+#     land:row[8] , 
+#     price_and_pepayment:row[9] , 
+#     building_and_structure:row[10]  , 
+#     floor_plan:row[11]  , 
+#     surrounding_environment:row[12]  , 
+#     family_relatives:row[13] , 
+#     other_point:row[14] 
+#   )
+# end
 
-PropertyList.create(property_type:"新築戸建")
-PropertyList.create(property_type:"中古戸建")
-PropertyList.create(property_type:"新築マンション")
-PropertyList.create(property_type:"中古マンション")
-PropertyList.create(property_type:"土地のみ")
+# PropertyList.create(property_type:"新築戸建")
+# PropertyList.create(property_type:"中古戸建")
+# PropertyList.create(property_type:"新築マンション")
+# PropertyList.create(property_type:"中古マンション")
+# PropertyList.create(property_type:"土地のみ")
 
-ConsultationMatter.create(item:"価格・返済")
-ConsultationMatter.create(item:"建物・構造")
-ConsultationMatter.create(item:"間取り")
-ConsultationMatter.create(item:"周辺環境")
-ConsultationMatter.create(item:"家族・親族")
-ConsultationMatter.create(item:"その他")
+# ConsultationMatter.create(item:"価格・返済")
+# ConsultationMatter.create(item:"建物・構造")
+# ConsultationMatter.create(item:"間取り")
+# ConsultationMatter.create(item:"周辺環境")
+# ConsultationMatter.create(item:"家族・親族")
+# ConsultationMatter.create(item:"その他")
 
-Customer.create(name: "kokyaku1", mail: "kokyaku1@mail.com")
-Customer.create(name: "kokyaku2", mail: "kokyaku2@mail.com")
+Customer.create(name: "kokyaku1", mail: "kokyaku1@mail.com", specialist_id: 1)
+Customer.create(name: "kokyaku2", mail: "kokyaku2@mail.com", specialist_id: 2)
 
-Property.create(property_type: 1, url: "url/bukkenjouhou1.jp", other: "sonota1", trader: "XXhome", price: 30000, newly_built_house:1 , used_house:0 , newly_built_condominium:0 , used_condominium:0 , land:0 , price_and_pepayment:1 , building_and_structure:0 , floor_plan:1 , surrounding_environment:0 , family_relatives:1, other_point:0, customer_id:1 )
-Property.create(property_type: 2, url: "url/bukkenjouhou2.jp", other: "sonota2", trader: "XXhome", price: 50000, newly_built_house:0 , used_house:1 , newly_built_condominium:0 , used_condominium:0 , land:0 , price_and_pepayment:1 , building_and_structure:0 , floor_plan:0 , surrounding_environment:1 , family_relatives:0, other_point:0, customer_id:2 )
-Property.create(property_type: 3, url: "url/bukkenjouhou3.jp", other: "sonota3", trader: "XXhome", price: 110000, newly_built_house:0 , used_house:0 , newly_built_condominium:1 , used_condominium:0 , land:0 , price_and_pepayment:0 , building_and_structure:0 , floor_plan:1 , surrounding_environment:1 , family_relatives:0, other_point:0 )
+# Property.create(property_type: 1, url: "url/bukkenjouhou1.jp", other: "sonota1", trader: "XXhome", price: 30000, newly_built_house:1 , used_house:0 , newly_built_condominium:0 , used_condominium:0 , land:0 , price_and_pepayment:1 , building_and_structure:0 , floor_plan:1 , surrounding_environment:0 , family_relatives:1, other_point:0, customer_id:1 )
+# Property.create(property_type: 2, url: "url/bukkenjouhou2.jp", other: "sonota2", trader: "XXhome", price: 50000, newly_built_house:0 , used_house:1 , newly_built_condominium:0 , used_condominium:0 , land:0 , price_and_pepayment:1 , building_and_structure:0 , floor_plan:0 , surrounding_environment:1 , family_relatives:0, other_point:0, customer_id:2 )
+# Property.create(property_type: 3, url: "url/bukkenjouhou3.jp", other: "sonota3", trader: "XXhome", price: 110000, newly_built_house:0 , used_house:0 , newly_built_condominium:1 , used_condominium:0 , land:0 , price_and_pepayment:0 , building_and_structure:0 , floor_plan:1 , surrounding_environment:1 , family_relatives:0, other_point:0 )
+Property.create(property_type: 1, url: "url/bukkenjouhou1.jp", other: "sonota1", trader: "XXhome", price: 30000, newly_built_house:1 , used_house:0 , newly_built_condominium:0 , used_condominium:0 , land:0 , price_and_pepayment:1 , building_and_structure:0 , floor_plan:1 , surrounding_environment:0 , family_relatives:1, other_point:0, customer_id:3 )
